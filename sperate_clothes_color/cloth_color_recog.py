1d917ee571cb6a9b9caf49d300876e466e1a3df7
roseonghwan <nsh1998@kookmin.ac.kr>
2021-07-21 22:21:31 +0900

    seperate clothes color without background

--- sperate_clothes_color/cloth_color_recog.py
+++ sperate_clothes_color/cloth_color_recog.py
@@ -44,41 +44,34 @@ def plot_colors(hist, centroids):
     # each cluster
     percent_list = []
     color_list = []
-    color_list2 = []
+    # 배경은 퍼센트 부분에 포함x
     for (percent, color) in zip(hist, centroids):
         # plot the relative percentage of each cluster
         if int(color.astype("uint8").tolist()[0]) == int(color.astype("uint8").tolist()[1]) == int(
                 color.astype("uint8").tolist()[2]) == 0:
-            print("$")
+            pass
         else:
             percent_list.append(percent)
-    print("&", sum(percent_list))
     bar = np.zeros((50, int(300 * (sum(percent_list))), 3), dtype="uint8")
     startX = 0
+
+    # histogram에서 배경색 제외
     for (percent, color) in zip(hist, centroids):
         # plot the relative percentage of each cluster
         endX = startX + (percent * 300)
         if int(color.astype("uint8").tolist()[0]) == int(color.astype("uint8").tolist()[1]) == int(
                 color.astype("uint8").tolist()[2]) == 0:
-            print("$")
+            pass
         else:
             cv2.rectangle(bar, (int(startX), 0), (int(endX), 50), color.astype("uint8").tolist(), -1)
             startX = endX
-            percent_list.append(percent * 100)
             color_list.append(color)
-    print("&", sum(percent_list))
-    cv2.rectangle(bar, (int(startX), 0), (300, 50), (255, 255, 255), -1)
-    for i in color_list:
-        print("*", i)
-        if int(i[0]) == int(i[1]) == int(i[2]) == 0:
-            print("^", i)
-        else:
-            color_list2.append(i)
+
     # return the bar chart
-    return bar, percent_list, color_list2
+    return bar, percent_list, color_list
 
 
-def image_color_cluster(image, k=5):
+def image_color_cluster(image, k=8):
     # image = cv2.imread(image_path)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image = image.reshape((image.shape[0] * image.shape[1], 3))
@@ -86,26 +79,20 @@ def image_color_cluster(image, k=5):
     clt.fit(image)
     hist = centroid_histogram(clt)
     bar, p_list, c_list = plot_colors(hist, clt.cluster_centers_)
-    # print(p_list)
-    # print(c_list)
+    print(p_list)
+    print(c_list)
     plt.figure()
     plt.axis("off")
     plt.imshow(bar)
     plt.show()
-    return c_list
+    return c_list, p_list
 
 
 if __name__ == '__main__':
     picture = cv2.imread('orange.png')
     img = extraction(picture)
     plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-    rgb_list = image_color_cluster(img)
-
+    rgb_list, percent_list = image_color_cluster(img)
     color_list = color_data.extract_color(rgb_list)
-    priority_color = color_list[0]
-    print(color_list)
+    priority_color = color_list[percent_list.index(max(percent_list))]
     print("====", priority_color, "====")
-    # for i in range(len(rgb_list)):
-    #     for j in range(len(rgb_list[i])):
-    #         print(i, '-', j, ': ', int(rgb_list[i][j]))
-    #     print("\n")
